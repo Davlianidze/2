3757edaed94333f44c0bf125202d81fb3c6460ab
Davlianidze <d.davli2007@gmail.com>
2023-06-19 11:54:26 +0400

    first commit

--- main.lua
+++ main.lua
@@ -1,55 +1,152 @@
+push = require 'push'
+Class = require 'class'
 
-WINDOW_WIDTH = 1288
-WINDOW_HEIGHT = 728
+
+local enemy = require "Enemy"
+local button = require "Button"
+
+math.randomseed(os.time())
+
+local game = {
+    difficulty = 1,
+    state = {
+        menu = true,
+        paused = false,
+        running = false,
+        ended = false
+    },
+    points = 0,
+    levels = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
+}
+
+
+local fonts = {
+    medium = {
+        font = love.graphics.newFont(16),
+        size = 16
+    },
+    large = {
+        font = love.graphics.newFont(24),
+        size = 24
+    },
+    massive = {
+        font = love.graphics.newFont(60),
+        size = 60
+    }
+}
+
+local player = {
+    radius = 20,
+    x = 30,
+    y = 30
+}
+
+local buttons = {
+    menu_state = {},
+    ended_state = {} 
+}
+
+local enemies = {}
+
+local function changeGameState(state)
+    game.state["menu"] = state == "menu"
+    game.state["paused"] = state == "paused"
+    game.state["running"] = state == "running"
+    game.state["ended"] = state == "ended"
+end
+
+local function startNewGame()
+    changeGameState("running")
+    game.points = 0
+
+    enemies = {
+        enemy(1)
+    } 
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    if not game.state["running"] then
+        if button == 1 then
+            if game.state["menu"] then
+                for index in pairs(buttons.menu_state) do
+                    buttons.menu_state[index]:checkPressed(x, y, player.radius)
+                end
+            elseif game.state["ended"] then 
+                for index in pairs(buttons.ended_state) do 
+                    buttons.ended_state[index]:checkPressed(x, y, player.radius)
+                end
+            end
+        end
+    end
+end
 
 function love.load()
-    player1 = {}
-    player1.x = 400
-    player1.y = 200
-    player1.speed = 3
+    love.mouse.setVisible(false)
+    love.window.setTitle("Save the Ball!")
 
-    player2 = {}
-    player2.x = 200
-    player2.y = 100
-    player2.speed = 3
+    buttons.menu_state.play_game = button("Play Game", startNewGame, nil, 120, 40)
+    buttons.menu_state.settings = button("Settings", nil, nil, 120, 40)
+    buttons.menu_state.exit_game = button("Exit Game", love.event.quit, nil, 120, 40)
 
 
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
-        fullscreen = false, 
-        resizable = true 
-    })
-  end
-  
-function love.update(dt)
-    if love.keyboard.isDown("d") then
-        player1.x = player1.x + player1.speed
-    elseif love.keyboard.isDown("a") then
-        player1.x = player1.x - player1.speed
-    end
-  
-    if love.keyboard.isDown("w") then
-        player1.y = player1.y - player1.speed
-    elseif love.keyboard.isDown("s") then
-        player1.y = player1.y + player1.speed
-    end
-
-    if love.keyboard.isDown("right") then
-		  player2.x = player2.x + 3
-    elseif love.keyboard.isDown("left") then
-		  player2.x = player2.x - 3
-	  end
-  
-	  if love.keyboard.isDown("up") then
-		  player2.y = player2.y - 3
-    elseif love.keyboard.isDown("down") then
-		  player2.y = player2.y + 3
-	  end
-
+    buttons.ended_state.replay_game = button("Replay", startNewGame, nil, 100, 50)
+    buttons.ended_state.menu = button("Menu", changeGameState, "menu", 100, 50)
+    buttons.ended_state.exit_game = button("Quit", love.event.quit, nil, 100, 50)
 end
-  
-  function love.draw()
-    love.graphics.setBackgroundColor(0, 0, 255)
-    love.graphics.circle('fill', player1.x, player1.y, 20)
-    love.graphics.circle('fill', player2.x, player2.y, 20)
-  end
-  
+
+function love.update(dt)
+    player.x, player.y = love.mouse.getPosition()
+
+    if game.state["running"] then
+        for i = 1, #enemies do
+            if not enemies[i]:checkTouched(player.x, player.y, player.radius) then
+                enemies[i]:move(player.x, player.y)
+
+                for i = 1, #game.levels do
+                    if math.floor(game.points) == game.levels[i] then
+                        table.insert(enemies, 1, enemy(game.difficulty * (i + 1)))
+                        game.points = game.points + 1
+                    end
+                end
+            else
+                changeGameState("ended")
+            end
+        end
+
+        game.points = game.points + dt
+    end
+end
+
+function love.draw()
+    love.graphics.setFont(fonts.medium.font)
+
+    love.graphics.printf("FPS: " .. love.timer.getFPS(), fonts.medium.font, 10, love.graphics.getHeight() - 30, love.graphics.getWidth())
+
+    if game.state["running"] then
+        
+        love.graphics.printf(math.floor(game.points), fonts.large.font, 0, 10, love.graphics.getWidth(), "center")
+
+        for i = 1, #enemies do
+            enemies[i]:draw()
+        end
+
+        love.graphics.circle("fill", player.x, player.y, player.radius)
+    elseif game.state["menu"] then
+        buttons.menu_state.play_game:draw(10, 20, 17, 10)
+        buttons.menu_state.settings:draw(10, 70, 17, 10)
+        buttons.menu_state.exit_game:draw(10, 120, 17, 10)
+    elseif game.state["ended"] then 
+        love.graphics.setFont(fonts.large.font)
+
+        buttons.ended_state.replay_game:draw(love.graphics.getWidth() / 2.25, love.graphics.getHeight() / 1.8, 10, 10)
+        buttons.ended_state.menu:draw(love.graphics.getWidth() / 2.25, love.graphics.getHeight() / 1.53, 17, 10)
+        buttons.ended_state.exit_game:draw(love.graphics.getWidth() / 2.25, love.graphics.getHeight() / 1.33, 22, 10)
+
+
+        love.graphics.printf(math.floor(game.points), fonts.massive.font, 0, love.graphics.getHeight() / 2 - fonts.massive.size, love.graphics.getWidth(), "center")
+    end
+
+    if not game.state["running"] then
+        love.graphics.circle("fill", player.x, player.y, player.radius / 2)
+    end
+end
